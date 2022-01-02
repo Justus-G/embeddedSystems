ba0b9bed9823b4f549f0eaab83ef5bbfea32b739
Justus Gillmann <gillmann@students.uni-marburg.de>
2022-01-02 17:03:04 +0100

    working code of I2S Audio receiving and serial plotting

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,54 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_I2CDevice.h>
 #include <driver/i2s.h>
-
+#include "driver/i2s.h"
 const i2s_port_t I2S_PORT = I2S_NUM_0;
-const int BLOCK_SIZE = 32;
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
-    Serial.println("Configuring I2S...");
     esp_err_t err;
 
-    // code from https://github.com/maspetsberger/esp32-i2s-mems/blob/master/examples/VUMeterDemo/VUMeterDemo.ino
-    // The I2S config as per the example
     const i2s_config_t i2s_config = {
-        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX), // Receive, not transfer
+        .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = 16000,                         // 16KHz
-        // 8 bits -> error (invalid bits per sample)
-        // 16 bits -> nur -1
-        // 24 bits -> nur -256
-        // 32 bits -> nur -1
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_24BIT, // could only get it to work with 32bits
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT, // although the SEL config should be left, it seems to transmit on right
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
         .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,     // Interrupt level 1
-        .dma_buf_count = 8,                           // number of buffers
-        .dma_buf_len = BLOCK_SIZE                     // samples per buffer
+        .dma_buf_count = 4,                           // number of buffers
+        .dma_buf_len = 8                              // 8 samples per buffer (minimum)
     };
 
-    // The pin config as per the setup
     const i2s_pin_config_t pin_config = {
-        .bck_io_num = 19,   // BCKL
-        .ws_io_num = 26,    // LRCL
-        .data_out_num = -1, // not used (only for speakers)
-        .data_in_num = 23   // DOUT
+        .bck_io_num = 14,   // Serial Clock (SCK)
+        .ws_io_num = 13,    // Word Select (WS)
+        .data_out_num = I2S_PIN_NO_CHANGE, // not used (only for speakers)
+        .data_in_num = 12   // Serial Data (SD)
     };
 
-    // Configuring the I2S driver and pins.
-    // This function must be called before any I2S driver read/write operations.
     err = i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
     if (err != ESP_OK) {
         Serial.printf("Failed installing driver: %d\n", err);
         while (true);
     }
+
     err = i2s_set_pin(I2S_PORT, &pin_config);
     if (err != ESP_OK) {
         Serial.printf("Failed setting pin: %d\n", err);
         while (true);
     }
-    Serial.println("I2S driver installed.");
 }
 
-void loop()
-{
-    int32_t samples[BLOCK_SIZE];
-    size_t num_bytes_read;
-    esp_err_t err = i2s_read(I2S_PORT, 
-                            (char *)samples, 
-                            BLOCK_SIZE,     // the doc says bytes, but its elements.
-                            &num_bytes_read,
-                            portMAX_DELAY); // no timeout
-    
-    if (err != ESP_OK) {
-        Serial.printf("Failed reading from mic: %d\n", err);
-        while (true);
+void loop() {
+    // Read a single sample and log it for the Serial Plotter.
+    int32_t sample = 0;
+    int bytes_read = i2s_pop_sample(I2S_PORT, (char *)&sample, portMAX_DELAY); // no timeout
+    if (bytes_read > 0) {
+        Serial.println(sample);
+    } else {
+        Serial.println("no samples read.");
     }
-
-    int samples_read = num_bytes_read / 8;
-    for (int i = 0; i < samples_read; ++i) {
-        Serial.printf("%d", samples[i]);
-        if(i != (samples_read - 1))
-            Serial.print(", ");
-    }
-    Serial.println();
 }
