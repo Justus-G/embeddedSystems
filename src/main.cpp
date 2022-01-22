59fb53fdd9efd8922a9b1de54575676689153d52
Justus Gillmann <gillmann@students.uni-marburg.de>
2022-01-22 13:27:08 +0100

    first try of FFT

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,29 @@
 #include <Adafruit_SSD1306.h>
 #include <driver/i2s.h>
 #include "driver/i2s.h"
-const i2s_port_t I2S_PORT = I2S_NUM_0;
+#include <arduinoFFT.h>
+
 
 void myAudioMethod (void * pvParameters);
-void myLEDMethod (void * pvParameters);
+void myAnalyzeMethod (void * pvParameters);
+
+// Audio stuff
+const i2s_port_t I2S_PORT = I2S_NUM_0;
+const uint16_t sample_count = 64;
+uint16_t sample_counter = 0;
+const int sampling_frequency = 16000; // Hz
+unsigned int sampling_period_us;
+
 
 long last_sample = -1;
+double samples_real[sample_count];
+double samples_imaginary[sample_count];
+double samples_real_fft[sample_count];
+double samples_imaginary_fft[sample_count];
+
+bool calculate_fft_now = false;
+
+arduinoFFT FFT = arduinoFFT();
 
 //OLED pins
 #define OLED_SDA 21
@@ -30,10 +47,12 @@ void setup() {
     oledDisplay.clearDisplay();  // Display(puffer) löschen
     oledDisplay.setTextSize(1);  // kleine Schriftgröße (Höhe 8px)
     oledDisplay.setTextColor(WHITE);  // helle Schrift, dunkler Grund)
+    oledDisplay.display();
 
     // I2S Audio init stuff
     Serial.begin(115200);
     esp_err_t err;
+    sampling_period_us = round(1000000*(1.0/sampling_frequency));
 
     const i2s_config_t i2s_config = {
         .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
@@ -43,7 +62,7 @@ void setup() {
         .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,     // Interrupt level 1
         .dma_buf_count = 4,                           // number of buffers
-        .dma_buf_len = 8                              // 8 samples per buffer (minimum)
+        .dma_buf_len = 1024                           // 8 samples per buffer (minimum)
     };
 
     const i2s_pin_config_t pin_config = {
@@ -82,7 +101,7 @@ void setup() {
     delay(500); 
 
     xTaskCreatePinnedToCore(
-        myLEDMethod,
+        myAnalyzeMethod,
         "Task2",     /* name of task. */
         10000,       /* Stack size of task */
         NULL,        /* parameter of the task */
@@ -94,43 +113,101 @@ void setup() {
 }
 
 void myAudioMethod( void * pvParameters ){
-    /*Serial.print("Task1 running on core ");
-    Serial.println(xPortGetCoreID());*/
+    unsigned long microseconds = micros();
 
     while(true) {
         // Read a single sample and log it for the Serial Plotter.
         int32_t sample = 0;
-        int bytes_read = i2s_pop_sample(I2S_PORT, (char *)&sample, portMAX_DELAY); // no timeout
-        if (bytes_read > 0) {
-            Serial.println(sample);
-            last_sample = sample;
-        } else {
+        //int bytes_read = i2s_pop_sample(I2S_PORT, (char *)&sample, portMAX_DELAY); // no timeout
+        size_t bytes_read;
+        i2s_read(I2S_PORT, (char *)&sample, 4, &bytes_read, portMAX_DELAY);
+        if (bytes_read <= 0) {
             Serial.println("no samples read.");
+            while(true);   
         }
+
+        //Serial.println(sample);
+        last_sample = bytes_read;
+
+        samples_real[sample_counter] = sample;
+        samples_imaginary[sample_counter] = 0;
+        
+        sample_counter++;
+        if(sample_counter == 64) {
+            sample_counter = 0;
+            calculate_fft_now = true;
+        }
+        
+        // wait to match sample frequency
+        while(micros() - microseconds < sampling_period_us){
+        }
+        microseconds += sampling_period_us;
+
         vTaskDelay(1);
     }
 }
 
-void myLEDMethod( void * pvParameters ){
+
+void copy_samples() {
+    for(int i=0; i < sample_count; i++) {
+        samples_real_fft[i] = samples_real[i];
+        samples_imaginary_fft[i] = samples_imaginary[i];
+    }
+}
+
+
+void print_array(double array[]) {
+    for(int i = 0; i < sample_count; i++) {
+        Serial.println(array[i] / 1000);
+        //Serial.print(" ");
+    }
+    Serial.println();
+}
+
+
+
+void myAnalyzeMethod( void * pvParameters ){
     /*Serial.print("Task2 running on core ");
     Serial.println(xPortGetCoreID());*/
 
-    bool switcher = true;
     while(true) {
         oledDisplay.clearDisplay();  // Display(puffer) löschen
         oledDisplay.setCursor(0, 0);  // links oben anfangen
-        oledDisplay.println(last_sample);
-        oledDisplay.display();
+        
+        if(calculate_fft_now) {
+            calculate_fft_now = false;
+        } else {
+            continue;
+        }
+        // copy samples into safely mutable arrays
+        copy_samples();
 
-        /*if(switcher)
-            digitalWrite(25, HIGH);
-        else
-            digitalWrite(25, LOW);
-        switcher = !switcher;
-        delay(700);*/
+        Serial.println("samples before:");
+        print_array(samples_real_fft);
+        
+        // weigh data with Hamming weighting
+        FFT.Windowing(samples_real_fft, sample_count, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+
+        // compute FFT
+        FFT.Compute(samples_real_fft, samples_imaginary_fft, sample_count, FFT_FORWARD);
+
+        // to magnitudes
+        FFT.ComplexToMagnitude(samples_real_fft, samples_imaginary_fft, sample_count);
+
+        Serial.println("samples after:");
+        print_array(samples_real_fft);
+        //print_array(samples_imaginary_fft);
+        //Serial.println("---");
+
+        //oledDisplay.println(samples_real_fft[0]);
+        //oledDisplay.display();
+        
         vTaskDelay(1);
     }
 }
+
+
+
 
 void loop() {
     vTaskDelete(NULL);
