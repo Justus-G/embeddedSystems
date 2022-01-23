f695cce6365a2d1d6c18c2685a6551df16cd7953
Justus Gillmann <gillmann@students.uni-marburg.de>
2022-01-24 00:47:49 +0100

    implement graphical representation of RTA, still need to do some adjustments and improve timing

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,26 @@ const int i2s_read_size = 4 * sample_count; // 4 bytes per sample * #samples
 int32_t samples[sample_count] = {0};        // destination of each i2s reading
 
 // FFT stuff
-double_t fft_imag[sample_count] = {};       // FFT imaginary input values (all 0)
-double_t fft_real[sample_count] = {};       // FFT real input values are copied from samples[] before each FFT analysis
+double fft_imag[sample_count] = {};         // FFT imaginary input values (all 0)
+double fft_real[sample_count] = {};         // FFT real input values are copied from samples[] before each FFT analysis
 arduinoFFT FFT = arduinoFFT(fft_real, fft_imag, sample_count, i2s_sampling_rate);
+const int fft_frequency_bin_count = 16;     // indices:                    0     1     2     3     4     5     6      7      8      9     10     11     12     13      14      15
+const int16_t fft_frequency_bins[fft_frequency_bin_count]              = {100,  140,  196,  274,  384,  538,  753,  1054,  1476,  2066,  2893,  4050,  5669,  7937,  11112,  15557}; // Hz
+const int16_t fft_frequency_bin_lower_indices[fft_frequency_bin_count] = {0,    1,    2,    3,    4,    5,    8,    11,    15,    21,    29,    40,    56,    79,    111,    155};
+const int16_t fft_frequency_bin_upper_indices[fft_frequency_bin_count] = {  1,    2,    3,    4,    5,    8,   11,    15,    21,    29,    40,    56,    79,   111,    155,    256};
+const float_t fft_frequency_bin_multiplier[fft_frequency_bin_count] =    {0.001,0.005, 0.3, 0.5,  0.7,  0.9,  1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,   1.0,    1.0,    1.0};
 
 // parallelization stuff
 bool i2s_read_done = false;                 // core synchronization
-void read_i2s (void * pvParameters);        // i2s read method
-void analyze_data (void * pvParameters);    // FFT analyze method
+void read_i2s(void * pvParameters);         // i2s read method
+void analyze_data(void * pvParameters);     // FFT analyze method
 
 // OLED Display stuff
-#define OLED_SDA 21
-#define OLED_SCL 22 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_SDA 21                         // SDA pin
+#define OLED_SCL 22                         // SCL pin
+#define SCREEN_WIDTH 128                    // OLED display width, in pixels
+#define SCREEN_HEIGHT 32                    // OLED display height, in pixels
+#define SCREEN_ADDRESS 0x3C                 // 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 oledDisplay(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
@@ -111,11 +116,6 @@ void setup() {
     );
     delay(500);
 
-
-    oledDisplay.clearDisplay();
-    oledDisplay.println("go!");
-    oledDisplay.display();
-
     start_time = millis();
 }
 
@@ -189,14 +189,83 @@ void read_i2s(void * pvParameters) {
 
         vTaskDelay(1);
 
-        Serial.print("read ");
-        Serial.println(micros() - time);
+        Serial.print("read took ");
+        Serial.print(micros() - time);
+        Serial.println("µs");
         time = micros();
     }
 }
 
+const int fft_normalization_store_size = 20;
+double fft_normalization_max_values[fft_normalization_store_size] = {};
+int fft_normalization_max_value_index = 0;
+
+double* get_binned_values(double fft_data[], double *result) {
+    double max_value = 0, min_value = __INT_MAX__;
+    for(int result_index = 0; result_index < fft_frequency_bin_count; result_index++) {
+        int index_min = fft_frequency_bin_lower_indices[result_index];
+        int index_max = fft_frequency_bin_upper_indices[result_index];
+        
+        result[result_index] = 0;   // directly save max value in result[]
+        for(int bin_index = index_min; bin_index < index_max; bin_index++) {
+            double_t value = fft_data[bin_index] * fft_frequency_bin_multiplier[result_index];
+            if(value > result[result_index]) {
+                result[result_index] = value;
+            }
+            if(value > max_value) {
+                max_value = value;
+            }
+            if(value < min_value) {
+                min_value = value;
+            }
+        }
+    }
+
+    // store current max value
+    fft_normalization_max_values[fft_normalization_max_value_index] = max_value;
+
+    
+
+    // calculate average max value
+    double avg_max_value = 0;
+    for(int i = 0; i < fft_normalization_max_value_index; i++) {
+        avg_max_value += fft_normalization_max_values[i];
+    }
+    avg_max_value /= fft_normalization_store_size;
+
+    fft_normalization_max_value_index++;
+    if(fft_normalization_max_value_index >= fft_normalization_store_size) {
+        fft_normalization_max_value_index = 0;
+    }
+
+    // debug: print fft data
+    /*Serial.print("fft data: ");
+    for(int i = 0; i < 10; i++) {
+        Serial.print(fft_data[i]);
+        Serial.print(", ");
+    }
+    Serial.println();*/
+
+    // normalize data
+    //Serial.println("binned values:");
+    max_value -= min_value;
+    for(int result_index = 0; result_index < fft_frequency_bin_count; result_index++) {
+        result[result_index] = (result[result_index] - min_value) / max_value;
+        if(result[result_index] > 1.0) {
+            result[result_index] = 1.0;
+        }
+        // debug: print result
+        //Serial.print(result[result_index], 16);
+        //Serial.print(", ");
+    }
+    //Serial.println();
+
+    return result;
+}
+
 // analzye takes 15900µs = 15,9ms
 // without FFT Hamming-windowing: 12,2ms
+// with RTA display: 30ms
 void analyze_data(void * pvParameters) {
 
     // OLED stuff
@@ -228,12 +297,12 @@ void analyze_data(void * pvParameters) {
         // fill fft_imag array and convert input array from int32_t to double_t
         for(int i = 0; i < sample_count; i++) {
             fft_imag[i] = 0;
-            fft_real[i] = (double_t) samples[i];
+            fft_real[i] = (double) samples[i];
         }
 
         i2s_read_done = false;
 
-        //FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+        FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD);
         FFT.Compute(FFT_FORWARD);
         FFT.ComplexToMagnitude();
 
@@ -245,7 +314,7 @@ void analyze_data(void * pvParameters) {
         }
         Serial.println();*/
 
-        int freq = FFT.MajorPeakParabola();
+        /*int freq = FFT.MajorPeakParabola();
 
         char c[15];
         String s = "";
@@ -264,9 +333,29 @@ void analyze_data(void * pvParameters) {
         oledDisplay.print("analyze ");
         oledDisplay.print(micros() - time_end);
         oledDisplay.println("");
+        oledDisplay.display();*/
+
+        oledDisplay.clearDisplay();
+        oledDisplay.setCursor(0, 0);
+        double binned_data[fft_frequency_bin_count] = {};
+        get_binned_values(fft_real, binned_data);
+        const float rta_max_bar_height = 32.0;
+        for(int i = 0; i < fft_frequency_bin_count; i++) {
+            int x_offset = i * 8;
+            double height_d = std::floor((binned_data[i] * rta_max_bar_height) + 0.5);
+            int height = height_d;
+            int y_offset = rta_max_bar_height - height;
+            for(int x = 0; x < 8; x++) {
+                oledDisplay.drawFastVLine(x_offset + x, y_offset, 32, WHITE);
+            }
+        }
         oledDisplay.display();
 
         vTaskDelay(1);
+
+        Serial.print("analyze took ");
+        Serial.print(micros() - time_end);
+        Serial.println("µs");
 
         time_end = micros();
 
@@ -283,3 +372,18 @@ void analyze_data(void * pvParameters) {
 void loop() {
     vTaskDelete(NULL);
 }
+
+
+/*
+TODO: 
+    - queue hinzufügen -> queue geht nicht, weil FFT nicht unterschiedlich viele Elemente erwarten kann
+    --> Ringpuffer hinzufügen, feste Größe z.b. 4
+        - i2s_read() pusht gelesene samples in puffer
+        - analyze() popt alle verfügbaren (4) samples aus queue und berechnet FFT
+            - FFT auf 4 * samples_count anpassen (vorher angucken, ob 4 auf einander folgende sample-arrays aneinander passen)
+            - eventuell Semaphore, dass analyze() immer exakt nach 4 neuen sample-Arrays ausgeführt wird
+    - Normalisierung des RTA über gespeicherten durchschnitts-max-value
+        - max-value nutzen, evtl. Anzahl der gespeicherten max-Values noch erhöhen (20 -> 200 ?)
+    - RTA-multiplier anpassen
+
+*/
