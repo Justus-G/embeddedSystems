3767675b837f19211f09a6f44684015d5bd295eb
Justus Gillmann <gillmann@students.uni-marburg.de>
2022-02-07 00:19:50 +0100

    move issues from source code to README

--- src/main.cpp
+++ src/main.cpp
@@ -412,31 +412,3 @@ void loop() {
     vTaskDelete(NULL);
 }
 
-
-/*
-DONE:
-    - CSV-Output automatisch an/aus schalten, wenn SD-Karte eingelegt bzw. nicht eingelegt ist
-    - Normalisierung des RTA über gespeicherten durchschnitts-max-value
-        - max-value nutzen, evtl. Anzahl der gespeicherten max-Values noch erhöhen (20 -> 200 ?)
-    - RTA-multiplier anpassen
-
-WON'T DO:
-    - eventuell Durchschnitt statt Maximum der einzelnen FFT-Bins für RTA-bin-Auswertung nehmen 
-        (Maximum ist schon gut so)
-    - eventuell RTA-Frequenz-Bins anpassen:
-        - weniger bins < 200Hz
-        - mehr bins 250Hz - 2k Hz
-        - weniger > 10k Hz
-        (nicht mehr unbedingt notwendig seit angepassten RTA-Multiplier)
-( eventuell:
-    - queue hinzufügen -> queue geht nicht, weil FFT nicht unterschiedlich viele Elemente erwarten kann
-    --> Ringpuffer hinzufügen, feste Größe z.b. 4       (aktuell wird nur jedes 2. bis 3. gelesene sample analysiert)
-        - i2s_read() pusht gelesene samples in puffer
-        - analyze() popt alle verfügbaren (4) samples aus queue und berechnet FFT
-            - FFT auf 4 * samples_count anpassen (vorher angucken, ob 4 auf einander folgende sample-arrays aneinander passen)
-            - eventuell Semaphore, dass analyze() immer exakt nach 4 neuen sample-Arrays ausgeführt wird
-)
-
-OBSOLET:
-    - herausfinden, warum Display manchmal für ca 0,25s schwarz ist
-*/
